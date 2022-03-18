5c9b5f3e131913180b6d265d5dbb1b9e842f561c
Steve Kieffer <sk@skieffer.info>
2022-03-18 15:36:15 -0400

    Remove `eval` option for parsing.
    The `eval_expr()` function is deleted.
    The `parse_expr()` function can only use the `ControlledEvaluator`.
    
    In this commit we have some logging code, with which we can determine all the calls that need to be allowed,
    to get through the unit tests.

--- sympy/core/sympify.py
+++ sympy/core/sympify.py
@@ -99,7 +99,7 @@ def _convert_numpy_types(a, **sympify_args):
 
 
 def sympify(a, locals=None, convert_xor=True, strict=False, rational=False,
-        evaluate=None, controlled=True):
+        evaluate=None):
     """
     Converts an arbitrary expression to a type that can be used inside SymPy.
 
@@ -492,7 +492,7 @@ sympify() performed the string fallback resulting in the following string:
 
     try:
         a = a.replace('\n', '')
-        expr = parse_expr(a, local_dict=locals, transformations=transformations, evaluate=evaluate, controlled=controlled)
+        expr = parse_expr(a, local_dict=locals, transformations=transformations, evaluate=evaluate)
     except (TokenError, SyntaxError) as exc:
         raise SympifyError('could not parse %r' % a, exc)
 
